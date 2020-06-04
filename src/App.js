d346c7f7e8be1927583765b7ec56c3a866a97a31
Pedro Zago <pedroozago97@gmail.com>
2020-06-04 01:52:46 +0000

    Aula 19.05 - Começando o Front End.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './api';
 
 function App() {
-  return <h1>ReactJS, Template!</h1>
+
+    const [filme, setFilme] = useState([]);
+
+    useEffect(() => {
+        api.get('/filme').then((response) => {
+            const itens = response.data;
+            setFilme(itens);
+        })
+    }, [])
+
+    return (
+        <table>
+            {filme.map(item => (
+                <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.nome}</td>
+                    <td>{item.diretor}</td>
+                    <td>{item.ano}</td>
+                    <td>
+                        <input type="checkbox" checked={item.assistido}/>
+                    </td>
+                </tr>
+            ))}
+        </table>
+    );
+
 }
 
 export default App;
