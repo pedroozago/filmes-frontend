deb144655918a411d4853e55f8436591596f9c7d
Pedro Zago <pedroozago97@gmail.com>
2020-06-05 02:07:57 +0000

    Aula do dia 26/05 - Implementando Loading.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import api from './api';
+import { Link } from 'react-router-dom';
 
 function App() {
 
     const [filme, setFilme] = useState([]);
+    const [loading, setloading] = useState(true);
 
+    //executa para obter informações externas
     useEffect(() => {
         api.get('/filme').then((response) => {
             const itens = response.data;
             setFilme(itens);
+            setloading(false);
         })
     }, [])
 
     return (
-        <table>
-            {filme.map(item => (
-                <tr key={item.id}>
-                    <td>{item.id}</td>
-                    <td>{item.nome}</td>
-                    <td>{item.diretor}</td>
-                    <td>{item.ano}</td>
-                    <td>
-                        <input type="checkbox" checked={item.assistido}/>
-                    </td>
-                </tr>
-            ))}
-        </table>
+        <>
+            {loading ? <span>Carregando dados...</span> : <div/> }
+            <table>
+                {filme.map(item => (
+                    <tr key={item.id}>
+                        <td>{item.id}</td>
+                        <td>{item.nome}</td>
+                        <td>{item.diretor}</td>
+                        <td>{item.ano}</td>
+                        <td>
+                            <input type="checkbox" checked={item.assistido}/>
+                        </td>
+                    </tr>
+                ))}
+            </table>
+            <Link to="/create">Adicionar</Link>
+        </>
     );
 
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch> 
+            <Route path="/" exact>
+                <App/>
+            </Route>
+            <Route path="/create">
+                <h1>Cadastro</h1>
+            </Route>
+        </Switch>
+    </BrowserRouter>
+    , document.getElementById('root'));
